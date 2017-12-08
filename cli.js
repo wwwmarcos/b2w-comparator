3dbeaec96830f40685fab8f135a28897b3e3f0a2
Marcos Florencio <dstepbw@gmail.com>
2017-12-08 13:44:24 -0200

    feat: show result in a table

--- cli.js
+++ cli.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 
 const {buildPrices} = require('./lib')
+const Table = require('cli-table2')
 
 const baseUrl = process.argv[2]
 
-const logResults = result => console.log(JSON.stringify(result, null, 2))
+const logResults = result => {
+  const table = new Table()
+
+  result
+    .map(data => [data.shopName, data.product.name, data.product.price])
+    .forEach(data => table.push(data))
+
+  console.log(table.toString())
+}
 
 buildPrices(baseUrl, logResults)
